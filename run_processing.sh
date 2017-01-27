95743bae83cd3b089e61a45000f1cf44eed73b3f
Hannah Kates <hbk254@nyu.edu>
2017-01-27 14:09:11 -0500

    uncommenting rpcoessing

--- run_processing.sh
+++ run_processing.sh
@@ -5,151 +5,139 @@
 ## NOTE: This script requires that your setup the DATABASE_URL environment variable. 
 ## Directions are in the README.md.
 
-# ## Joining on source data info
-# psql $DATABASE_URL -f ./scripts_processing/join_sourcedatainfo.sql
+## Joining on source data info
+psql $DATABASE_URL -f ./scripts_processing/join_sourcedatainfo.sql
 
-# ## Standardizing borough and assigning borough code
-# psql $DATABASE_URL -f ./scripts_processing/standardize_borough.sql
+## Standardizing borough and assigning borough code
+psql $DATABASE_URL -f ./scripts_processing/standardize_borough.sql
 
-# ## Switching One to 1 for geocoding and removing invalid (string) address numbers
-# psql $DATABASE_URL -f ./scripts_processing/standardize_address.sql
+## Switching One to 1 for geocoding and removing invalid (string) address numbers
+psql $DATABASE_URL -f ./scripts_processing/standardize_address.sql
 
 ## CREATING GEOMETRIES WITH GEOCLIENT
 
-# ## Run the geocoding script using address and borough - get BBL, BIN, lat/long
-# echo 'Running geocoding script using address and borough...'
-# time node ./scripts_processing/address2geom_borough.js
-# echo 'Done geocoding using address and borough'
+## Run the geocoding script using address and borough - get BBL, BIN, lat/long
+echo 'Running geocoding script using address and borough...'
+time node ./scripts_processing/address2geom_borough.js
+echo 'Done geocoding using address and borough'
 
-# ## Run the geocoding script using address and zipcode - get BBL, BIN, lat/long
-# echo 'Running geocoding script using address and zip code...'
-# time node ./scripts_processing/address2geom_zipcode.js
-# echo 'Done geocoding using address and zip code'
+## Run the geocoding script using address and zipcode - get BBL, BIN, lat/long
+echo 'Running geocoding script using address and zip code...'
+time node ./scripts_processing/address2geom_zipcode.js
+echo 'Done geocoding using address and zip code'
 
-# # ## Run the geocoding script using place name (facilityname) - get BBL, BIN, lat/long
-# # echo 'Running geocoding script using place name and borough...'
-# # time node ./scripts_processing/place2geom_nameborough.js
-# # echo 'Done geocoding using place name and borough'
-# # # ^^ Hasn't been catching anything
+## FILLING IN STANDARDIZED ADDRESS AND LOCATION DETAILS WITH GEOCLIENT
 
-# # ## Run the geocoding script using place name (address) - get BBL, BIN, lat/long
-# # echo 'Running geocoding script using place name and zip code...'
-# # time node ./scripts_processing/place2geom_addressborough.js
-# # echo 'Done geocoding using place name and zip code'
-# # ## ^^ Hasn't been catching anything
+# Do spatial join with borough for remaining records with geoms that have blanks
+echo 'Forcing 2D...'
+psql $DATABASE_URL -f ./scripts_processing/force2D.sql
+echo 'Forced 2D complete'
+time psql $DATABASE_URL -f ./scripts_processing/boroughjoin.sql
+## ^ added this step because a lot of zipcode Geoclient searches weren't finding results
 
-# ## FILLING IN STANDARDIZED ADDRESS AND LOCATION DETAILS WITH GEOCLIENT
+# Run the geocoding script using address get BBL and BIN and standardize address
+echo 'Running geocoding script getting BBL using address and borough...'
+time node ./scripts_processing/address2bbl_borough.js
+echo 'Done getting BBL using address and borough'
 
-# # Do spatial join with borough for remaining records with geoms that have blanks
-# echo 'Forcing 2D...'
-# psql $DATABASE_URL -f ./scripts_processing/force2D.sql
-# echo 'Forced 2D complete'
-# time psql $DATABASE_URL -f ./scripts_processing/boroughjoin.sql
-# ## ^ added this step because a lot of zipcode Geoclient searches weren't finding results
+echo 'Running geocoding script getting BBL using address and zipcode...'
+time node ./scripts_processing/address2bbl_zipcode.js
+echo 'Done getting BBL using address and zipcode'
+# ^^ Hasn't been catching anything
 
-# # Run the geocoding script using address get BBL and BIN and standardize address
-# echo 'Running geocoding script getting BBL using address and borough...'
-# time node ./scripts_processing/address2bbl_borough.js
-# echo 'Done getting BBL using address and borough'
+## Standardizing borough and assigning borough code 
+## (Geoclient sometimes fills in Staten Is instead of Staten Island)
+psql $DATABASE_URL -f ./scripts_processing/standardize_borough.sql
 
-# echo 'Running geocoding script getting BBL using address and zipcode...'
-# time node ./scripts_processing/address2bbl_zipcode.js
-# echo 'Done getting BBL using address and zipcode'
-# # ^^ Hasn't been catching anything
+psql $DATABASE_URL -f ./scripts_processing/removeinvalidBIN.sql
+psql $DATABASE_URL -f ./scripts_processing/copy_backup1.sql
 
-# ## Standardizing borough and assigning borough code 
-# ## (Geoclient sometimes fills in Staten Is instead of Staten Island)
-# psql $DATABASE_URL -f ./scripts_processing/standardize_borough.sql
+## CREATING GEOMS USING BIN AND BBL CENTROID FOR REMAINING MISSING GEOMS
 
-# psql $DATABASE_URL -f ./scripts_processing/removeinvalidBIN.sql
-# psql $DATABASE_URL -f ./scripts_processing/copy_backup1.sql
+echo 'Joining on geometry using BIN...'
+time psql $DATABASE_URL -f ./scripts_processing/bin2geom.sql
+echo 'Done joining on geometry using BIN'
 
-# ## CREATING GEOMS USING BIN AND BBL CENTROID FOR REMAINING MISSING GEOMS
+echo 'Joining on geometry using BBL...'
+time psql $DATABASE_URL -f ./scripts_processing/bbl2geom.sql
+echo 'Done joining on geometry using BBL'
+# ^^ Hasn't been catching anything
 
-# echo 'Joining on geometry using BIN...'
-# time psql $DATABASE_URL -f ./scripts_processing/bin2geom.sql
-# echo 'Done joining on geometry using BIN'
+## FILLING IN MISSING ADDRESS INFO FROM PLUTO USING BBL WHEN GEOM EXISTS
 
-# echo 'Joining on geometry using BBL...'
-# time psql $DATABASE_URL -f ./scripts_processing/bbl2geom.sql
-# echo 'Done joining on geometry using BBL'
-# # ^^ Hasn't been catching anything
+echo 'Joining missing address info using BBL...'
+time psql $DATABASE_URL -f ./scripts_processing/bbl2address.sql
 
-# ## FILLING IN MISSING ADDRESS INFO FROM PLUTO USING BBL WHEN GEOM EXISTS
+Create backup table of records with no geom or outside NYC and then delete from database
 
-# echo 'Joining missing address info using BBL...'
-# time psql $DATABASE_URL -f ./scripts_processing/bbl2address.sql
+# PREPPING FOR SPATIAL JOINS WITH PLUTO
 
-# Create backup table of records with no geom or outside NYC and then delete from database
+## Create a spatial index for the facilities database and for MapPLUTO and VACUUM
+## (Reindex and vacuum after each spatial join)
 
-## PREPPING FOR SPATIAL JOINS WITH PLUTO
+echo 'Indexing and vacuuming facilities and dcp_mappluto...'
+psql $DATABASE_URL -f ./scripts_processing/force2D.sql
+psql $DATABASE_URL -f ./scripts_processing/setSRID_26918.sql
+psql $DATABASE_URL -f ./scripts_processing/vacuum.sql
+echo 'Done indexing and vacuuming facilities and dcp_mappluto'
 
-# ## Create a spatial index for the facilities database and for MapPLUTO and VACUUM
-# ## (Reindex and vacuum after each spatial join)
+## Do a spatial join with MapPLUTO to get BBL and addresses info if missing
 
-# echo 'Indexing and vacuuming facilities and dcp_mappluto...'
-# psql $DATABASE_URL -f ./scripts_processing/force2D.sql
-# psql $DATABASE_URL -f ./scripts_processing/setSRID_26918.sql
-# psql $DATABASE_URL -f ./scripts_processing/vacuum.sql
-# echo 'Done indexing and vacuuming facilities and dcp_mappluto'
+echo 'Spatially joining with dcp_mappluto...'
+time psql $DATABASE_URL -f ./scripts_processing/bbljoin.sql
+echo 'Done spatially joining with dcp_mappluto'
+psql $DATABASE_URL -f ./scripts_processing/vacuum.sql
 
-# ## Do a spatial join with MapPLUTO to get BBL and addresses info if missing
+psql $DATABASE_URL -f ./scripts_processing/standardize_address.sql
+# ^ need to clean up addresses again after filling in with PLUTO address
 
-# echo 'Spatially joining with dcp_mappluto...'
-# time psql $DATABASE_URL -f ./scripts_processing/bbljoin.sql
-# echo 'Done spatially joining with dcp_mappluto'
-# psql $DATABASE_URL -f ./scripts_processing/vacuum.sql
+echo 'Filling in missing BINS where there is a 1-1 relationship between BBL and BIN...'
+time psql $DATABASE_URL -f ./scripts_processing/bbl2bin.sql
 
-# psql $DATABASE_URL -f ./scripts_processing/standardize_address.sql
-## ^ need to clean up addresses again after filling in with PLUTO address
+## TABULAR JOINS WITH BUILDINGFOOTPRINTS AND PLUTO TO OVERWRITE GEOMS
 
-# echo 'Filling in missing BINS where there is a 1-1 relationship between BBL and BIN...'
-# time psql $DATABASE_URL -f ./scripts_processing/bbl2bin.sql
+echo 'Creating a backup copy before overwriting any geometries...'
+psql $DATABASE_URL -f ./scripts_processing/copy_backup2.sql
 
-# ## TABULAR JOINS WITH BUILDINGFOOTPRINTS AND PLUTO TO OVERWRITE GEOMS
+echo 'Overwriting geometry using BIN centroid...'
+time psql $DATABASE_URL -f ./scripts_processing/bin2overwritegeom.sql
+echo 'Done overwriting geometry using BIN centroid'
 
-# echo 'Creating a backup copy before overwriting any geometries...'
-# psql $DATABASE_URL -f ./scripts_processing/copy_backup2.sql
+echo 'Overwriting geometry using BBL centroid...'
+time psql $DATABASE_URL -f ./scripts_processing/bbl2overwritegeom.sql
+echo 'Done overwriting geometry using BBL centroid'
 
-# echo 'Overwriting geometry using BIN centroid...'
-# time psql $DATABASE_URL -f ./scripts_processing/bin2overwritegeom.sql
-# echo 'Done overwriting geometry using BIN centroid'
+# ## Convert back to 4326, calculating lat,long and x,y for all blank records, and create ID for all records at once to use for deduping
 
-# echo 'Overwriting geometry using BBL centroid...'
-# time psql $DATABASE_URL -f ./scripts_processing/bbl2overwritegeom.sql
-# echo 'Done overwriting geometry using BBL centroid'
-
-# # ## Convert back to 4326, calculating lat,long and x,y for all blank records, and create ID for all records at once to use for deduping
-
-# psql $DATABASE_URL -f ./scripts_processing/setSRID_4326.sql
-# echo 'Calculating x,y for all blank records...'
-# time psql $DATABASE_URL -f ./scripts_processing/calcxy.sql
-# echo 'Done calculating x,y for all blank records'
+psql $DATABASE_URL -f ./scripts_processing/setSRID_4326.sql
+echo 'Calculating x,y for all blank records...'
+time psql $DATABASE_URL -f ./scripts_processing/calcxy.sql
+echo 'Done calculating x,y for all blank records'
 
 
-# echo 'Spatially joining with neighborhood boundaries...'
-# time psql $DATABASE_URL -f ./scripts_processing/cdjoin.sql
-# time psql $DATABASE_URL -f ./scripts_processing/ntajoin.sql
-# time psql $DATABASE_URL -f ./scripts_processing/zipcodejoin.sql
-# time psql $DATABASE_URL -f ./scripts_processing/counciljoin.sql
-# time psql $DATABASE_URL -f ./scripts_processing/tractjoin.sql
+echo 'Spatially joining with neighborhood boundaries...'
+time psql $DATABASE_URL -f ./scripts_processing/cdjoin.sql
+time psql $DATABASE_URL -f ./scripts_processing/ntajoin.sql
+time psql $DATABASE_URL -f ./scripts_processing/zipcodejoin.sql
+time psql $DATABASE_URL -f ./scripts_processing/counciljoin.sql
+time psql $DATABASE_URL -f ./scripts_processing/tractjoin.sql
 
-# # psql $DATABASE_URL -f ./scripts_processing/addID.sql
+# psql $DATABASE_URL -f ./scripts_processing/addID.sql
 
-# ## FINAL FORMATTING
+## FINAL FORMATTING
 
-# # ## Final formatting -- find and properly capitalize acronyms and abbreviations
+# ## Final formatting -- find and properly capitalize acronyms and abbreviations
 
-# echo 'Cleaning up capitalization...'
-# time psql $DATABASE_URL -f ./scripts_processing/fixallcaps.sql
-# echo 'Done cleaning up capitalization'
-# psql $DATABASE_URL -f ./scripts_processing/vacuum.sql
+echo 'Cleaning up capitalization...'
+time psql $DATABASE_URL -f ./scripts_processing/fixallcaps.sql
+echo 'Done cleaning up capitalization'
+psql $DATABASE_URL -f ./scripts_processing/vacuum.sql
 
-# # Create backup table before merging and dropping duplicates
-# echo 'Creating backup before merging and dropping duplicates...'
-# psql $DATABASE_URL -f ./scripts_processing/copy_backup3.sql
+# Create backup table before merging and dropping duplicates
+echo 'Creating backup before merging and dropping duplicates...'
+psql $DATABASE_URL -f ./scripts_processing/copy_backup3.sql
 
-# # # DEDUPING
+# # DEDUPING
 
 ## Merge Child Care and Pre-K Duplicate records
 echo 'Merging and dropping Child Care and Pre-K duplicates...'
@@ -174,19 +162,12 @@ psql $DATABASE_URL -f ./scripts_processing/duplicates_colp_relatedlots_merged.sq
 psql $DATABASE_URL -f ./scripts_processing/duplicates_colp_relatedlots_colponly.sql
 echo 'Deduped!'
 
-# ## Remove DPR properties sourced from COLP that overlap with records from DPR's data
-# echo 'Updating DPR records with attributes from properties that came from COLP...'
-# time psql $DATABASE_URL -f ./scripts_processing/update_ParkDupsFromCOLP.sql
+## Final export to csv that excludes null geoms and geoms outside NYC
 
-# echo 'Removing overlapping DPR properties that came from COLP...'
-# time psql $DATABASE_URL -f ./scripts_processing/delete_ParkDupsFromCOLP.sql
-
-# ## Final export to csv that excludes null geoms and geoms outside NYC
-
-# echo 'Exporting...'
-# time psql $DATABASE_URL -f ./scripts_processing/export.sql
+echo 'Exporting...'
+time psql $DATABASE_URL -f ./scripts_processing/export.sql
 # sh ./scripts_processing/export_json.sh
-# echo 'All done!'
+echo 'All done!'
 
 
 
@@ -195,9 +176,19 @@ echo 'Deduped!'
 ###########################################################################################
 
 ## TO DO:
-## Need to fix the script that assigns a record to its closest BBL and BIN
-## Running BBL through GeoClient may no longer be necessary since info can be 
-## filled in using a spatial join now that the join is faster
+
+# # ## Run the geocoding script using place name (facilityname) - get BBL, BIN, lat/long
+# # echo 'Running geocoding script using place name and borough...'
+# # time node ./scripts_processing/place2geom_nameborough.js
+# # echo 'Done geocoding using place name and borough'
+# # # ^^ Hasn't been catching anything
+
+# # ## Run the geocoding script using place name (address) - get BBL, BIN, lat/long
+# # echo 'Running geocoding script using place name and zip code...'
+# # time node ./scripts_processing/place2geom_addressborough.js
+# # echo 'Done geocoding using place name and zip code'
+# # ## ^^ Hasn't been catching anything
+
 
 # # ## 5. Run the geocoding script using intersection - get BBL, BIN, lat/long
 # node ./scripts_processing/intersection2geom.js
