6f152546f8c438ca1c139f1862b374d76507a902
Hannah Kates <hannah.b.kates@gmail.com>
2017-07-10 11:18:15 -0400

    replaced db URL variable

--- 3_geoprocessing.sh
+++ 3_geoprocessing.sh
@@ -8,14 +8,14 @@
 ## PREPPING DATA
 
 echo 'Creating a backup copy before doing any geoprocessing...'
-# time psql $DATABASE_URL -f ./3_geoprocessing/copy_backup0.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/copy_backup0.sql
 echo 'Forcing 2D...'
-time psql $DATABASE_URL -f ./3_geoprocessing/force2D.sql
+time psql -d facdb -U dbadmin -f ./3_geoprocessing/force2D.sql
 echo 'Setting SRID, indexing, and vacuuming facilities and dcp_mappluto...'
-time psql $DATABASE_URL -f ./3_geoprocessing/setSRID_4326.sql
-time psql $DATABASE_URL -f ./3_geoprocessing/vacuum.sql
+time psql -d facdb -U dbadmin -f ./3_geoprocessing/setSRID_4326.sql
+time psql -d facdb -U dbadmin -f ./3_geoprocessing/vacuum.sql
 echo 'Spatial join with boroughs...'
-time psql $DATABASE_URL -f ./3_geoprocessing/join_boro_pregeoclient.sql
+time psql -d facdb -U dbadmin -f ./3_geoprocessing/join_boro_pregeoclient.sql
 ## This joins borough onto records that do not have borough or zip code already assigned. This distinction is important because some records came with coordinates that were the result of incorrect geocoding. There are records that have addresses that exist in multiple boroughs, and there are cases where a record comes with boro=Manhattan but it's coordinates are at the location at the same address that's in Brooklyn instead. We want to be careful to not override the borough values that were provided and should be skeptical of coordinates provided on the open data portal that were likely generated automatically without any QA/QC. We only want to fill in borough for the cases where no other borough or zipcode value was provided so we enable potential for finding matches in GeoClient what wouldn't be possible otherwise.
 
 ## GEOCLIENT
@@ -27,61 +27,61 @@ echo 'Running through GeoClient using address and zip code...'
 time node ./3_geoprocessing/geoclient_zipcode.js
 ## Standardizing borough and assigning borough code again because
 ## Geoclient sometimes fills in Staten Is instead of Staten Island
-time psql $DATABASE_URL -f ./3_geoprocessing/join_boro.sql
-time psql $DATABASE_URL -f ./3_geoprocessing/clean_invalidBIN.sql
-# time psql $DATABASE_URL -f ./3_geoprocessing/copy_backup1.sql
+time psql -d facdb -U dbadmin -f ./3_geoprocessing/join_boro.sql
+time psql -d facdb -U dbadmin -f ./3_geoprocessing/clean_invalidBIN.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/copy_backup1.sql
 
 # ## TABULAR JOIN WITH PLUTO FILLING IN MISSING ADDRESS INFO USING BBL WHEN GEOM EXISTS
 
 # echo 'Joining missing address info onto records using BBL...'
-# time psql $DATABASE_URL -f ./3_geoprocessing/join_PLUTOtabular.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/join_PLUTOtabular.sql
 
 # ## SPATIAL JOINS WITH PLUTO TO GET BBL AND OTHER MISSING INFO
 
 # echo 'Spatially joining with dcp_mappluto...'
-# time psql $DATABASE_URL -f ./3_geoprocessing/join_PLUTOspatial.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/join_PLUTOspatial.sql
 # echo 'Done spatially joining with dcp_mappluto'
-# time psql $DATABASE_URL -f ./3_geoprocessing/vacuum.sql
-# time psql $DATABASE_URL -f ./2_assembly/standardize_address.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/vacuum.sql
+# time psql -d facdb -U dbadmin -f ./2_assembly/standardize_address.sql
 # # ^ need to clean up addresses again after filling in with PLUTO address
 
 # ## FILLING IN REMAINING MISSING BINS
 
 # echo 'Filling in missing BINS where there is a 1-1 relationship between BBL and BIN...'
-# time psql $DATABASE_URL -f ./3_geoprocessing/bbl2bin.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/bbl2bin.sql
 # echo 'Creating a backup copy before overwriting any geometries...'
-# time psql $DATABASE_URL -f ./3_geoprocessing/copy_backup2.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/copy_backup2.sql
 
 # # TABULAR JOINS WITH BUILDINGFOOTPRINTS AND PLUTO TO OVERWRITE GEOMS WITH CENTROID
 
 # echo 'Overwriting geometry using BIN centroid...'
-# time psql $DATABASE_URL -f ./3_geoprocessing/bin2overwritegeom.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/bin2overwritegeom.sql
 # echo 'Done overwriting geometry using BIN centroid'
 
 # echo 'Overwriting geometry using BBL centroid...'
-# time psql $DATABASE_URL -f ./3_geoprocessing/bbl2overwritegeom.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/bbl2overwritegeom.sql
 # echo 'Done overwriting geometry using BBL centroid'
 
 # ## Calculating lat,long and x,y for all blank records
 # echo 'Calculating x,y for all blank records...'
-# time psql $DATABASE_URL -f ./3_geoprocessing/calcxy.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/calcxy.sql
 # echo 'Done calculating x,y for all blank records'
 
 # echo 'Spatially joining with neighborhood boundaries...'
-# time psql $DATABASE_URL -f ./3_geoprocessing/join_commboard.sql
-# time psql $DATABASE_URL -f ./3_geoprocessing/join_nta.sql
-# time psql $DATABASE_URL -f ./3_geoprocessing/join_zipcode.sql
-# time psql $DATABASE_URL -f ./3_geoprocessing/clean_invalidZIP.sql
-# time psql $DATABASE_URL -f ./3_geoprocessing/clean_cityboro.sql
-# time psql $DATABASE_URL -f ./3_geoprocessing/join_council.sql
-# time psql $DATABASE_URL -f ./3_geoprocessing/join_censtract.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/join_commboard.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/join_nta.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/join_zipcode.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/clean_invalidZIP.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/clean_cityboro.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/join_council.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/join_censtract.sql
 # echo 'Spatially joining with COLP bbls to get propertytype...'
-# time psql $DATABASE_URL -f ./3_geoprocessing/join_proptype.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/join_proptype.sql
 # ## ^ In FacDB V1.5, will add conditional logic for type of facility
 # echo 'Setting propertytype for street plazas...'
-# time psql $DATABASE_URL -f ./3_geoprocessing/proptype_plazas.sql
-# time psql $DATABASE_URL -f ./3_geoprocessing/vacuum.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/proptype_plazas.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/vacuum.sql
 
 # ## Create backup table before merging and dropping duplicates
 # echo 'Creating backup before merging and dropping duplicates...'
-# time psql $DATABASE_URL -f ./3_geoprocessing/copy_backup3.sql
+# time psql -d facdb -U dbadmin -f ./3_geoprocessing/copy_backup3.sql
--- 4_deduping.sh
+++ 4_deduping.sh
@@ -8,62 +8,62 @@
 
 # Merge Child Care and Pre-K Duplicate records
 echo 'Merging and dropping Child Care and Pre-K duplicates...'
-time psql $DATABASE_URL -f ./4_deduping/duplicates_ccprek_acs_hhs.sql
-time psql $DATABASE_URL -f ./4_deduping/duplicates_removeFAKE.sql
-time psql $DATABASE_URL -f ./4_deduping/duplicates_ccprek_doe_acs.sql
-time psql $DATABASE_URL -f ./4_deduping/duplicates_removeFAKE.sql
-time psql $DATABASE_URL -f ./4_deduping/duplicates_ccprek_doe_dohmh.sql
-time psql $DATABASE_URL -f ./4_deduping/duplicates_removeFAKE.sql
-time psql $DATABASE_URL -f ./4_deduping/duplicates_ccprek_acs_dohmh.sql
-time psql $DATABASE_URL -f ./4_deduping/duplicates_removeFAKE.sql
-time psql $DATABASE_URL -f ./4_deduping/duplicates_ccprek_dohmh.sql
-time psql $DATABASE_URL -f ./4_deduping/duplicates_removeFAKE.sql
-time psql $DATABASE_URL -f ./4_deduping/copy_backup4.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_ccprek_acs_hhs.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_removeFAKE.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_ccprek_doe_acs.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_removeFAKE.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_ccprek_doe_dohmh.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_removeFAKE.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_ccprek_acs_dohmh.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_removeFAKE.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_ccprek_dohmh.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_removeFAKE.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/copy_backup4.sql
 
 echo 'Merging and dropping remaining duplicates, pre-COLP...'
-time psql $DATABASE_URL -f ./4_deduping/duplicates_remaining.sql
-time psql $DATABASE_URL -f ./4_deduping/duplicates_removeFAKE.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_remaining.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_removeFAKE.sql
 
 echo 'Merging and dropping remaining duplicates, pre-COLP...'
-time psql $DATABASE_URL -f ./4_deduping/duplicates_sfpsd_relatedlots.sql
-time psql $DATABASE_URL -f ./4_deduping/duplicates_removeFAKE.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_sfpsd_relatedlots.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_removeFAKE.sql
 
 echo 'Creating backup before merging and dropping COLP duplicates...'
-time psql $DATABASE_URL -f ./4_deduping/copy_backup5.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/copy_backup5.sql
 
 echo 'Merging and dropping COLP duplicates by BIN...'
-time psql $DATABASE_URL -f ./4_deduping/duplicates_colp_bin.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_colp_bin.sql
 echo 'Cleaning up remaining dummy values used for array_agg'
-time psql $DATABASE_URL -f ./4_deduping/duplicates_removeFAKE.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_removeFAKE.sql
 
 echo 'Merging and dropping COLP duplicates by BBL...'
-time psql $DATABASE_URL -f ./4_deduping/duplicates_colp_bbl.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_colp_bbl.sql
 echo 'Cleaning up remaining dummy values used for array_agg'
-time psql $DATABASE_URL -f ./4_deduping/duplicates_removeFAKE.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_removeFAKE.sql
 
 echo 'Merging related COLP duplicates on surrounding BBLs...'
-time psql $DATABASE_URL -f ./4_deduping/duplicates_colp_relatedlots_merged.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_colp_relatedlots_merged.sql
 echo 'Cleaning up remaining dummy values used for array_agg'
-time psql $DATABASE_URL -f ./4_deduping/duplicates_removeFAKE.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_removeFAKE.sql
 
 echo 'Merging remaining COLP duplicates on surrounding BBLs Part 1...'
-time psql $DATABASE_URL -f ./4_deduping/duplicates_colp_relatedlots_colponly_p1.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_colp_relatedlots_colponly_p1.sql
 echo 'Cleaning up remaining dummy values used for array_agg'
-time psql $DATABASE_URL -f ./4_deduping/duplicates_removeFAKE.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_removeFAKE.sql
 
 echo 'Merging remaining COLP duplicates on surrounding BBLs Part 2...'
-time psql $DATABASE_URL -f ./4_deduping/duplicates_colp_relatedlots_colponly_p2.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_colp_relatedlots_colponly_p2.sql
 echo 'Cleaning up remaining dummy values used for array_agg'
-time psql $DATABASE_URL -f ./4_deduping/duplicates_removeFAKE.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_removeFAKE.sql
 
 # Merge records that are exactly the same from the same data source
 echo 'Merging and dropping records that are exactly the same from the same data source...'
-time psql $DATABASE_URL -f ./4_deduping/duplicates_exactsame.sql
-time psql $DATABASE_URL -f ./4_deduping/duplicates_removeFAKE.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_exactsame.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/duplicates_removeFAKE.sql
 
 echo 'Deduped!'
 
 echo 'Cleaning up duplicates in BIN and BBl arrays...'
-time psql $DATABASE_URL -f ./4_deduping/removeArrayDuplicates.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/removeArrayDuplicates.sql
 
-time psql $DATABASE_URL -f ./4_deduping/copy_backup6.sql
+time psql -d facdb -U dbadmin -f ./4_deduping/copy_backup6.sql
