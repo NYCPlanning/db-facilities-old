2f332ebadf265349a4b523439e49a3c565a632d9
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2017-10-04 12:06:44 -0400

    fixing field names

--- 2_assembly/standardize/standardize_capacity.sql
+++ 2_assembly/standardize/standardize_capacity.sql
@@ -9,20 +9,20 @@ UPDATE facdb_utilization AS f
     SET 
 		util = NULL
 	WHERE
-		util,',' LIKE '% 0%'
+		util LIKE '% 0%'
 		AND capacity IS NULL
 		;
 
 UPDATE facdb_capacity AS f
     SET 
-		captype = NULL
+		capacitytype = NULL
 	WHERE
 		capacity IS NULL
 		;
 
 UPDATE facdb_utilization AS f
     SET 
-		utilrate = NULL
+		utiltype = NULL
 	WHERE
 		util IS NULL
 		;
@@ -31,15 +31,14 @@ UPDATE facdb_area AS f
     SET 
 		area =
 			(CASE
-				WHEN area[1]::numeric <> 0 THEN ROUND(area[1]::numeric,3)
+				WHEN area::numeric <> 0 THEN ROUND(area::numeric,3)
 				ELSE NULL
 			END),
 		areatype =
 			(CASE
-				WHEN area[1]::numeric <> 0 THEN areatype
+				WHEN area::numeric <> 0 THEN areatype
 				ELSE NULL
 			END)
 	WHERE
 		area IS NOT NULL
-		AND area NOT LIKE CONCAT('%', datasource, '%')
 		;
