96b7d76976041fb051e6d9dfe15d1729a5c44ee0
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2017-12-06 18:17:21 -0500

    analysis to report out at FacType level

--- analysis/sql/QA_facilityclassifications.sql
+++ analysis/sql/QA_facilityclassifications.sql
@@ -3,4 +3,11 @@ SELECT DISTINCT facdomain, facgroup, facsubgrp, COUNT(*)
 FROM facilities
 GROUP BY facdomain, facgroup, facsubgrp
 ORDER BY facsubgrp, facgroup, facdomain
-) TO '/prod/facilities-db/analysis/output/facdb_summarystats_facilityclassifications.csv' DELIMITER ',' CSV HEADER;
+) TO '/prod/db-facilities/analysis/output/facdb_summarystats_facilityclassifications.csv' DELIMITER ',' CSV HEADER;
+
+COPY(
+	SELECT DISTINCT pgtable, factype, facsubgrp, facgroup, facdomain, COUNT(*)
+	FROM facilities
+	GROUP BY pgtable, factype, facsubgrp, facgroup, facdomain
+	ORDER BY facsubgrp, facgroup, facdomain
+) TO '/prod/db-facilities/analysis/output/facdb_qaqc_facilityclassifications.csv' DELIMITER ',' CSV HEADER;
