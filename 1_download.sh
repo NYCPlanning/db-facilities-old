d12725e3ea06586e5408f288e93d501d4cb4bfff
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-10-09 14:59:24 -0400

    adding new scripts to pull from open data

--- 1_download.sh
+++ 1_download.sh
@@ -28,6 +28,7 @@ node loader.js install dcp_ntaboundaries
 node loader.js install doitt_zipcodes
 
 ## Data you probably don't have loaded
+node loader.js install acs_facilities_daycareheadstart
 node loader.js install bic_facilities_tradewaste
 node loader.js install dca_facilities_operatingbusinesses
 node loader.js install dcla_facilities_culturalinstitutions
@@ -37,8 +38,13 @@ node loader.js install dfta_facilities_contracts
 node loader.js install doe_facilities_busroutesgarages
 node loader.js install doe_facilities_universalprek
 node loader.js install dohmh_facilities_daycare
+node loader.js install dot_parkingfacilities
 node loader.js install dpr_parksproperties
+node loader.js install dycd_facilities_afterschoolprograms
+
 node loader.js install hhc_facilities_hospitals
+node loader.js install hra_centers
+
 node loader.js install nycha_facilities_policeservice
 node loader.js install nysdec_facilities_lands
 node loader.js install nysdec_facilities_solidwaste
@@ -48,18 +54,18 @@ node loader.js install nysomh_facilities_mentalhealth
 node loader.js install nysopwdd_facilities_providers
 node loader.js install nysparks_facilities_historicplaces
 node loader.js install nysparks_facilities_parks
+node loader.js install sbs_facilities_workforce1
 node loader.js install usdot_facilities_airports
 node loader.js install usdot_facilities_ports
 node loader.js install usnps_facilities_parks
 
+
 echo 'Done loading open source datasets. Moving on to "other" datasets...'
 ## Other_datasets - PULLING FROM FTP SITE
-node loader.js install acs_facilities_daycareheadstart
 node loader.js install dcas_facilities_colp
 node loader.js install dhs_facilities_shelters
 node loader.js install doe_facilities_schoolsbluebook
 node loader.js install dot_facilities_pedplazas
-node loader.js install dot_facilities_parkingfacilities
 node loader.js install dot_facilities_bridgehouses
 node loader.js install dot_facilities_ferryterminalslandings
 node loader.js install dot_facilities_mannedfacilities
@@ -74,9 +80,7 @@ node loader.js install nysed_nonpublicenrollment ## Actually is open but in xlsx
 node loader.js install omb_facilities_libraryvisits
 # node loader.js install dycd_facilities_compass
 # node loader.js install dycd_facilities_otherprograms
-node loader.js install dycd_facilities_afterschoolprograms
-node loader.js install hra_facilities_centers
-node loader.js install sbs_facilities_workforce1
+# node loader.js install hra_facilities_centers
 
 echo 'Done loading other source datasets'
 cd '../facilities-db'
