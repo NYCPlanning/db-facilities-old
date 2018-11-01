0fdfc03cc7083d563070075d0a469cec98e88d3c
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-11-01 16:25:01 -0400

    tweaking code

--- 6_qaqc/qc_frequencychangesource.sql
+++ 6_qaqc/qc_frequencychangesource.sql
@@ -1,17 +1,17 @@
 -- Reports how many records for the new version of the facilities table datasource and the old version
 -- Calculate the percentage change from the old version to the new version
 DROP TABLE IF EXISTS frequencychangesource;
-WITH newfrequency(dataname) AS (
-  SELECT dataname,
-        COUNT(dataname) AS newcount
+WITH newfrequency(pgtable) AS (
+  SELECT pgtable,
+        COUNT(pgtable) AS newcount
   FROM facilities
-  GROUP BY dataname),
-oldfrequency(dataname) AS (
-  SELECT dataname,
-        COUNT(dataname) AS oldcount
+  GROUP BY pgtable),
+oldfrequency(pgtable) AS (
+  SELECT pgtable,
+        COUNT(pgtable) AS oldcount
   FROM dcp_facilities
-  GROUP BY dataname)
-SELECT a.dataname,
+  GROUP BY pgtable)
+SELECT a.pgtable,
       a.newcount,
       b.oldcount,
       -- 100.0 * (new- prev) / (new)
@@ -19,9 +19,9 @@ SELECT a.dataname,
 INTO frequencychangesource
 FROM newfrequency a
 JOIN oldfrequency b
-ON a.dataname = b.dataname
-GROUP BY a.dataname, a.newcount, b.oldcount
-ORDER BY a.dataname;
+ON a.pgtable = b.pgtable
+GROUP BY a.pgtable, a.newcount, b.oldcount
+ORDER BY a.pgtable;
 
 \copy (SELECT * FROM frequencychangesource) TO '/prod/db-facilities/output/qc_frequencychangesource.csv' DELIMITER ',' CSV HEADER;
 DROP TABLE IF EXISTS frequencychangesource;
--- 6_qaqc/qc_geocodingreport.sql
+++ 6_qaqc/qc_geocodingreport.sql
@@ -1,31 +1,17 @@
 -- Reports how many records of geom is not null in both the geoprocessing table and the export table
 DROP TABLE IF EXISTS geocodingreport;
-WITH staging(pgtable) AS (
-  SELECT pgtable,
-       count(*) AS initialrecords,
-       count(pgtable) AS recordswithgeom
-       FROM facilities_all
-       WHERE geom IS NOT NULL
-       GROUP BY pgtable),
-export(pgtable) AS (
-  SELECT pgtable,
-       count(*) AS exportwithgeom
-       FROM facilities
-       WHERE geom IS NOT NULL
-       GROUP BY pgtable)
-SELECT a.pgtable,
-      a.initialrecords,
-      a.recordswithgeom,
-      b.exportwithgeom
-INTO geocodingreport
-FROM staging a
-JOIN export b
-ON a.pgtable = ARRAY[b.pgtable]
-GROUP BY a.pgtable,
-      a.initialrecords,
-      a.recordswithgeom,
-      b.exportwithgeom
-ORDER BY a.pgtable;
+CREATE TABLE geocodingreport AS (
+WITH newcount as (
+  SELECT pgtable::text, COUNT(*) as newcount
+  FROM facilities
+  GROUP BY pgtable::text),
+oldcount as (
+  SELECT pgtable::text, COUNT(*) as oldcount
+  FROM dcp_facilities
+  GROUP BY pgtable::text)
+SELECT a.pgtable, newcount, oldcount FROM newcount a
+LEFT JOIN oldcount b
+ON a.pgtable=b.pgtable);
 
 \copy (SELECT * FROM geocodingreport) TO '/prod/db-facilities/output/qc_geocodingreport.csv' DELIMITER ',' CSV HEADER;
 DROP TABLE IF EXISTS geocodingreport;
